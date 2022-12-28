047ccbd8d21ab817d1708cea797b6a86d10f6e08
IBakaev <ibakaev1999@gmail.com>
2022-12-29 00:46:03 +0300

    Working with Files

---
+++ Working with files/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.IO;
+
+namespace Working_with_files
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double sum = 0;
+            int temp = 0;            
+            Random random = new Random();
+            StreamWriter f = new StreamWriter($"{random.Next(-255, 255)}.txt");
+            Console.WriteLine("Введите количество чисел: ");
+            Console.WriteLine("----------------------------------");
+            int numbers = Convert.ToInt32(Console.ReadLine());
+            f.WriteLine("Сгенерированные числа: ");
+            f.WriteLine("----------------------------------");
+            for (int i = 0; i <= numbers - 1; i++)
+            {
+                temp = random.Next(-10, 10);
+                f.WriteLine($"{temp}");
+                sum += Math.Pow(temp, 2);
+            }
+            f.WriteLine("----------------------------------");
+            f.WriteLine($"Сумма квадратов чисел = {sum}");
+            f.WriteLine("----------------------------------");
+            f.Close();
+            Console.WriteLine("----------------------------------");
+            Console.WriteLine("Для просмотра результатов перейдите в сгенерированный текстовый файл.");
+        }
+    }
+}
