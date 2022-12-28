4fe4f37535964329270e202a7188c57d7ee5df5d
IBakaev <ibakaev1999@gmail.com>
2022-12-29 01:54:34 +0300

    OddNumbers counter

---
+++ OddNumbers/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace OddNumbers
+{
+    class Program
+    {
+        void OddCounter(char[] array, out int counter)
+        {
+            counter = 0;
+            for (int i = 0; i < array.Length; i++)
+            {
+                if (array[i] % 2 != 0)
+                    counter++;
+            }
+        }
+        static void Main(string[] args)
+        {
+            var obj = new Program();
+            Console.WriteLine("Введите натуральное число: ");
+            string someNumber = Console.ReadLine();
+            char[] chArray = someNumber.ToCharArray();
+            obj.OddCounter(chArray, out int counter);
+            Console.WriteLine($"Количество нечётных цифр в числе {someNumber}: {counter}");
+        }
+    }
+}
