127749885640e32d9ee6b808264ddba722bb2546
IBakaev <ibakaev1999@gmail.com>
2023-01-05 07:49:29 +0300

    SimpleRadioApp

---
+++ RadioApplication/RadioApp/Form1.Designer.cs
@@ -0,0 +1,112 @@
+﻿
+namespace RadioApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.axWindowsMediaPlayer1 = new AxWMPLib.AxWindowsMediaPlayer();
+            this.button1 = new System.Windows.Forms.Button();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            ((System.ComponentModel.ISupportInitialize)(this.axWindowsMediaPlayer1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // axWindowsMediaPlayer1
+            // 
+            this.axWindowsMediaPlayer1.Enabled = true;
+            this.axWindowsMediaPlayer1.Location = new System.Drawing.Point(34, 33);
+            this.axWindowsMediaPlayer1.Name = "axWindowsMediaPlayer1";
+            this.axWindowsMediaPlayer1.OcxState = ((System.Windows.Forms.AxHost.State)(resources.GetObject("axWindowsMediaPlayer1.OcxState")));
+            this.axWindowsMediaPlayer1.Size = new System.Drawing.Size(498, 243);
+            this.axWindowsMediaPlayer1.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.White;
+            this.button1.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button1.Font = new System.Drawing.Font("Georgia", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button1.Location = new System.Drawing.Point(405, 292);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(127, 25);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Играть";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.BackColor = System.Drawing.SystemColors.InactiveCaption;
+            this.comboBox1.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.comboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox1.Font = new System.Drawing.Font("Georgia", 9.75F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.comboBox1.ForeColor = System.Drawing.SystemColors.InactiveCaptionText;
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "Русское радио",
+            "Новое радио",
+            "Европа Плюс",
+            "Вести ФМ",
+            "Авторадио",
+            "Дорожное радио",
+            "Ретро FM",
+            "DFM",
+            "Юмор FM",
+            "Love Radio",
+            "Питер FM",
+            "Rock FM"});
+            this.comboBox1.Location = new System.Drawing.Point(34, 292);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(354, 24);
+            this.comboBox1.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
+            this.BackgroundImage = global::RadioApp.Properties.Resources._1663709348_31_phonoteka_org_p_abstraktnii_temnii_fon_oboi_32;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(568, 346);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.axWindowsMediaPlayer1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Radio Listener";
+            ((System.ComponentModel.ISupportInitialize)(this.axWindowsMediaPlayer1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private AxWMPLib.AxWindowsMediaPlayer axWindowsMediaPlayer1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ComboBox comboBox1;
+    }
+}
+
---
+++ RadioApplication/RadioApp/Form1.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace RadioApp
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            comboBox1.SelectedIndexChanged += comboBox1_SelectedIndexChanged;
+            comboBox1.SelectedIndex = 0;
+        }
+
+
+        string[] linksOnStations = { "https://rusradio.hostingradio.ru/rusradio96.aacp",
+            "https://icecast-newradio.cdnvideo.ru/newradio3",
+            "https://ep128.hostingradio.ru:8030/ep128",
+            "https://icecast-vgtrk.cdnvideo.ru/vestifm_mp3_128kbps",
+            "https://pub0202.101.ru:8443/stream/reg/mp3/128/region_avto_154",
+            "https://dorognoe.hostingradio.ru:8000/dorognoe",
+            "https://europaplus.hostingradio.ru:8014/retro320.mp3",
+            "https://dfm.hostingradio.ru/dfm96.aacp",
+            "https://pub0101.101.ru:8000/stream/air/aac/64/102",
+            "https://air2.volna.top/LoveRadio-MSK",
+            "https://icecast-piterfm.cdnvideo.ru/piterfm",
+            "https://nashe1.hostingradio.ru:80/rock-128.mp3"
+        };
+        int GetIndexOfSelectedItemInComboBox()
+        {
+            return comboBox1.FindStringExact(comboBox1.Text);
+        }
+
+        void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            string currentStation = comboBox1.SelectedItem.ToString();
+        }
+
+        string GetCurrentStation()
+        {
+            return comboBox1.Text;
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int currentIndexOfStation = GetIndexOfSelectedItemInComboBox();
+            //string currentStation;
+            string linkOnCurrentStation;
+            if (currentIndexOfStation < 0)
+            {
+                MessageBox.Show("Выберите радиостанцию!");
+            }
+            else
+            {
+                //currentStation = comboBox1.SelectedItem.ToString();
+                linkOnCurrentStation = linksOnStations[currentIndexOfStation];
+                axWindowsMediaPlayer1.URL = linkOnCurrentStation;
+            }
+        }
+    }
+}
